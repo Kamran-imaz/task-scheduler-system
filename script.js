0d8dcb757454b1fd6958d133b0ab6e05542a8432
Kamran-imaz <kamranimaz.professional@gmail.com>
2023-11-30 17:30:14 +0530

    styling completed

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-function addTask() {
-    const taskInput = document.getElementById('taskInput');
-    const dateTimeInput = document.getElementById('dateTimeInput');
-    const taskList = document.getElementById('taskList');
-  
-    const taskText = taskInput.value;
-    const dateTimeValue = dateTimeInput.value;
-  
-    if (taskText !== '' && dateTimeValue !== '') {
+function handleUniqueTask() {
+  const uniqueTaskInput = document.getElementById('uniqueTaskInput');
+  const uniqueDateTimeInput = document.getElementById('uniqueDateTimeInput');
+  const uniqueTaskList = document.getElementById('uniqueTaskList');
+
+  const uniqueTaskText = uniqueTaskInput.value;
+  const uniqueDateTimeValue = uniqueDateTimeInput.value;
+
+  if (uniqueTaskText !== '' && uniqueDateTimeValue !== '') {
       const li = document.createElement('li');
       li.innerHTML = `
-        <span>${taskText}</span>
-        <span>${dateTimeValue}</span>
-        <button onclick="deleteTask(this)">Delete</button>
-        <button onclick="setReminder('${taskText}', '${dateTimeValue}')">Set Reminder</button>
-      `;
-      taskList.appendChild(li);
-      taskInput.value = '';
-      dateTimeInput.value = '';
-    } else {
-      alert('Please enter a task and deadline!');
-    }
+    <span>${uniqueTaskText}</span>
+    <span>${uniqueDateTimeValue}</span>
+    <button onclick="removeUniqueTask(this)">Remove</button>
+    <button onclick="scheduleUniqueTask('${uniqueTaskText}', '${uniqueDateTimeValue}')">Schedule Task</button>
+  `;
+      uniqueTaskList.appendChild(li);
+      uniqueTaskInput.value = '';
+      uniqueDateTimeInput.value = '';
+  } else {
+      alert('Please enter a unique task and set a deadline!');
   }
-  
-  function deleteTask(element) {
-    const taskItem = element.parentElement;
-    taskItem.remove();
-  }
-  
-  function setReminder(taskText, dateTimeValue) {
-  
-    const now = new Date().getTime();
-    const selectedTime = new Date(dateTimeValue).getTime();
-    const timeDifference = selectedTime - now;
-  
-    if (timeDifference > 0) {
-      setTimeout(() => {
-        const notification = new Notification('Task Reminder', {
-          body: `Reminder: "${taskText}" is due now!`
-        });
-      }, timeDifference);
-    } else {
-      alert('This task is already past due.');
-    }
-  }
-  
+}
+
+function removeUniqueTask(element) {
+  const taskItem = element.parentElement;
+  taskItem.remove();
+}
+
+function scheduleUniqueTask(uniqueTaskText, uniqueDateTimeValue) {
   if (Notification.permission !== 'granted') {
-    Notification.requestPermission();
+      Notification.requestPermission();
+  } else {
+      const now = new Date().getTime();
+      const selectedTime = new Date(uniqueDateTimeValue).getTime();
+      const timeDifference = selectedTime - now;
+
+      if (timeDifference > 0) {
+          setTimeout(() => {
+              const notification = new Notification('Unique Task Reminder', {
+                  body: `Reminder: "${uniqueTaskText}" is due now!`
+              });
+          }, timeDifference);
+      } else {
+          alert('This unique task is already past due.');
+      }
   }
-  
+}
+
+if (Notification.permission !== 'granted') {
+  Notification.requestPermission();
+}
